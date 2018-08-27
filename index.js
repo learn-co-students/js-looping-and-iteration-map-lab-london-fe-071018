1e73c6d8e5091b7fe0d102ddbc4796b7dbecf491
Carmela Torres Molleda <carmela.torres@wework.com>
2018-08-27 15:50:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(driver => driver.toLowerCase());
+}
+
+function nameToAttributes(array){
+  return array.map(el => {
+    const splitName = el.split(" ");
+    const firstName = splitName[0];
+    const lastName = splitName[1];
+    return{firstName,lastName}
+  }) 
+}
+
+function attributesToPhrase(array) {
+  return array.map(el => `${el.name} is from ${el.hometown}`)
+}
